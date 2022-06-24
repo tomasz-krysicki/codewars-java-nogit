a78ab1c4b8ed5b77f08937b7ed4c4111d7b6473f
tomasz-krysicki <tomasz.krysicki@task.com.pl>
2022-06-24 13:22:59 +0200

    Method Math.ceil implemented

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        for (double i = 1; i < 13; i++) {
+            System.out.println((int) Math.ceil(i/3));
+        }
+
+        System.out.println(quarterOf(1));
+        System.out.println(quarterOf(2));
+        System.out.println(quarterOf(3));
+        System.out.println(quarterOf(4));
+        System.out.println(quarterOf(5));
+        System.out.println(quarterOf(6));
+        System.out.println(quarterOf(7));
+        System.out.println(quarterOf(8));
+        System.out.println(quarterOf(9));
+        System.out.println(quarterOf(10));
+        System.out.println(quarterOf(11));
+        System.out.println(quarterOf(12));
 
     }
+
+    public static int quarterOf(int month) {
+        return (int) Math.ceil((double)month / 3);
+    }
+
+
 }
